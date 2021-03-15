5ec01f28a83c0146be96ae64ed3c7bbb3cfdc763
stanislasbdx <perso@stan1712.com>
2021-03-15 12:57:39 +0100

    ✨feature: Changements graphiques (couleurs, formes)

--- window.py
+++ window.py
@@ -20,16 +20,21 @@ def initWindow():
 def addSign(canvas, type, x0, y0):
 	if(game.checkPosition(x0, y0) == False):
 		x0 = (x0 - 1)
+		y0 = (y0 - 1)
 
-	fill = "#BBB"
-	width = "2"
-	
-	if(type == "croix"):
-		canvas.create_line(x0 * cell_size + 10, y0 * cell_size + 10, (x0 + 1) * cell_size - 10, (y0 + 1) * cell_size - 10, fill=fill, width=width)
-		canvas.create_line((x0 + 1) * cell_size - 10, y0 * cell_size + 10, ((x0 + 1) - 1) * cell_size + 10, (y0 + 1) * cell_size - 10, fill=fill, width=width)
-	elif (type == "rond"): {
-		canvas.create_oval(x0 * cell_size + 5, y0 * cell_size + 5, (x0 + 1) * cell_size - 5, (y0 + 1) * cell_size - 5, outline=fill, width=width)
-	}
+		whiteFill = "WHITE"
+		blackFill = "BLACK"
+
+		width = "2"
+		
+		if(type == "N"):
+			"canvas.create_line(x0 * cell_size + 10, y0 * cell_size + 10, (x0 + 1) * cell_size - 10, (y0 + 1) * cell_size - 10, fill=fill, width=width)"
+			"canvas.create_line((x0 + 1) * cell_size - 10, y0 * cell_size + 10, ((x0 + 1) - 1) * cell_size + 10, (y0 + 1) * cell_size - 10, fill=fill, width=width)"
+			
+			canvas.create_oval(x0 * cell_size + 5, y0 * cell_size + 5, (x0 + 1) * cell_size - 5, (y0 + 1) * cell_size - 5, outline=blackFill, fill=blackFill, width=width)
+		elif (type == "B"): {
+			canvas.create_oval(x0 * cell_size + 5, y0 * cell_size + 5, (x0 + 1) * cell_size - 5, (y0 + 1) * cell_size - 5, outline=whiteFill, fill=whiteFill, width=width)
+		}
 
 		game.logSign(type, x0, y0)
 
@@ -44,7 +49,8 @@ def addSign(canvas, type, x0, y0):
 def initDamier(board_size):
 	canvas_size = cell_size * board_size
 	
-	colors = ["#2D1E2F", "#3A3657"]
+	#colors = ["#2D1E2F", "#3A3657"]
+	colors = ["#009067", "#02bd88"]
 	
 	gameWindow = Tk()
 	gameWindow.title("Othello")
@@ -66,10 +72,10 @@ def initDamier(board_size):
 	closeButton = Button(gameWindow, text='Quitter', command=gameWindow.destroy)
 	closeButton.pack()
 
-	addSign(canvas, "croix", 4, 4)
-	addSign(canvas, "croix", 5, 5)
+	addSign(canvas, "B", 4, 4)
+	addSign(canvas, "B", 5, 5)
 
-	addSign(canvas, "rond", 4, 5)
-	addSign(canvas, "rond", 5, 4)
+	addSign(canvas, "N", 4, 5)
+	addSign(canvas, "N", 5, 4)
 
 	gameWindow.mainloop()
